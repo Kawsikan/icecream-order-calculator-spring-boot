43a7a87b80fd60e943c3038bbf56007ace94c407
kawsikan <kawsinote@gmail.com>
2021-07-21 14:47:04 +0530

    initial commit

---
+++ src/main/java/com/order/icecream/IcecreamOrderCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.order.icecream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcecreamOrderCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcecreamOrderCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/order/icecream/IcecreamOrderCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.icecream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcecreamOrderCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
