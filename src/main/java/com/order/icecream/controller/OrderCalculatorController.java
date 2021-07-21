365e8f79e1f390233b0482ab598bd29a4f7d8810
kawsikan <kawsinote@gmail.com>
2021-07-21 16:33:38 +0530

    created ordercalculator controller

---
+++ src/main/java/com/order/icecream/controller/OrderCalculatorController.java
@@ -0,0 +1,9 @@
+package com.order.icecream.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/charge")
+public class OrderCalculatorController {
+}
